46e2ea4b8fa4c167be841697cd7ed37bd392aa10
rampemus <pasi.toivanen@live.com>
2020-04-15 17:46:13 +0300

    Initial values for form

--- mern-app-frontend/src/components/ContactForm.jsx
+++ mern-app-frontend/src/components/ContactForm.jsx
@@ -1,24 +1,47 @@
 import React from 'react'
 import { Formik, Field, Form } from 'formik'
 import ContactFieldArray from './ContactFieldArray'
+import contactService from '../services/contact'
 
 const ContactForm = (props) => {
 
+  const initialValues = props.initialValues || {
+    firstname: '',
+    lastname: '',
+    nickname: '',
+    title: '',
+    phone: [],
+    mobile: [],
+    email: [],
+    street: '',
+    postcode: '',
+    city: '',
+    country: ''
+  }
+
   return(
     <div className='contact-form'>
-      <h1>Contact form</h1>
+      <h2>Contact form</h2>
       <Formik
-        initialValues={{ phone: [], mobile: [], email: [] }}
-        onSubmit={(values, { setSubmitting })=>{
+        initialValues={initialValues}
+        enableReinitialize
+        onSubmit={(values, { setSubmitting }, setValues)=>{
           console.log('Sending values', values)
-          setSubmitting(false)
+          contactService.createContact(values).then(response => {
+            console.log('contact added', response)
+            setValues(initialValues)
+            setSubmitting(false)
+          }).catch(error => {
+            console.log('error while adding contact', error)
+            setSubmitting(false)
+          })
         }}
       >
         {({ isSubmitting, values, setValues }) => (
           <Form>
             <div className='contact-form-flexbox'>
               <div className='contact-form-panel'>
-                <p>Firstname: <Field type='text' name='firstname'/></p>
+                <p>Firstname: <Field type='text' name='firstname' value={values.firstname}/></p>
                 <p>Lastname: <Field type='text' name='lastname'/></p>
                 <p>Nickname: <Field type='text' name='nickname'/></p>
                 <p>Title: <Field type='text' name='title'/></p>
