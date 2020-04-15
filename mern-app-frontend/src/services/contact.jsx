f493e3e14645cb393489ea45ff24ca2fb1b1798b
rampemus <pasi.toivanen@live.com>
2020-04-15 17:02:01 +0300

    added create, modify and delete services

--- mern-app-frontend/src/services/contact.jsx
+++ mern-app-frontend/src/services/contact.jsx
@@ -6,4 +6,19 @@ const getAll = () => {
   return request.then(response => response.data)
 }
 
+const createContact = (contact) => {
+  const request = axios.post(baseUrl, contact)
+  return request.then(response => response.data)
+}
+
+const modifyContact = (contact) => {
+  const request = axios.put(baseUrl, contact)
+  return request.then(response => response.data)
+}
+
+const deleteContact = (contact) => {
+  const request = axios.put(`${baseUrl}/${contact.id}`)
+  return request.then(response => response.data)
+}
+
 export default { getAll }
