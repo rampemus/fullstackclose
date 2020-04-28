049ef8651aa97853d26342c595a9db63601eda2e
rampemus <pasi.toivanen@live.com>
2020-04-28 18:44:34 +0300

    added contact model

---
+++ mongo-app/models/contact.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose'
+
+const Schema = mongoose.Schema
+
+const ContactSchema = new Schema({
+  owner: String,
+  firstname: String,
+  lastname: String,
+  nickname: String,
+  title: String,
+  phone: [String],
+  mobile: [String],
+  email: [String],
+  street: String,
+  postcode: String,
+  city: String,
+  country: String
+})
+
+const Contact = mongoose.model('Contact', ContactSchema)
+
+export default Contact
