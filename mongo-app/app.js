601e53c20f5907829a9eb0abf4f401a45a2f4b9d
rampemus <pasi.toivanen@live.com>
2020-04-28 18:45:12 +0300

    App uses contact controller

--- mongo-app/app.js
+++ mongo-app/app.js
@@ -1,8 +1,13 @@
 import express from 'express'
 import coolRouter from './controllers/cool.js'
+import { getContact } from './controllers/contactController.js'
+import bodyparser from 'body-parser'
 
 const app = express()
 
+app.use(bodyparser.json())
+
 app.use('/api', coolRouter)
+app.use('/contact', getContact)
 
 export default app
