ced6a0a7bb6dce4f2698c7c345da392dc67f1f28
rampemus <pasi.toivanen@live.com>
2020-04-28 12:27:12 +0300

    Connected mongoose

--- mongo-app/index.js
+++ mongo-app/index.js
@@ -1,10 +1,15 @@
 import express from 'express'
 import coolRouter from './controllers/cool.js'
+import mongoose from 'mongoose'
 
 console.log('Hello there')
 
 const port = 3000
 const app = express()
+mongoose.connect('mongodb://localhost',{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.use('/api', coolRouter)
 
