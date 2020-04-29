b2a0f5f482ed69e5ef150918864a1c7f2bc9041b
rampemus <pasi.toivanen@live.com>
2020-04-29 11:22:05 +0300

    Cool removed

--- mongo-app/app.js
+++ mongo-app/app.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import coolRouter from './controllers/cool.js'
 import bodyparser from 'body-parser'
 import contactRouter from './routes/contact.js'
 import genreRouter from './routes/genre.js'
@@ -8,8 +7,6 @@ import authorRouter from './routes/author.js'
 const app = express()
 
 app.use(bodyparser.json())
-
-app.use('/api', coolRouter)
 
 app.use('/contact', contactRouter)
 
