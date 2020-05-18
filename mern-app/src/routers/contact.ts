eca29801d0c2b7e7c828e0dcbb5e85350542e9a7
rampemus <pasi.toivanen@live.com>
2020-05-18 14:06:20 +0300

    Fixed code formatting

--- mern-app/src/routers/contact.ts
+++ mern-app/src/routers/contact.ts
@@ -1,5 +1,10 @@
 import { Router } from 'express'
-import { contactPost, contactGet, contactPut, contactDelete } from '../controllers/contact'
+import {
+  contactPost,
+  contactGet,
+  contactPut,
+  contactDelete
+} from '../controllers/contact'
 
 const contactRouter = Router()
 
